30ec686c3a0f64ed3c31639d94c79166a55abf46
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-01-25 04:09:38 +0100

    CREATE A LOGIN TABLE IN THE DATABASE
    	Implemented a table called login which will be used to verify
    users.

---
+++ DB/login.sql
@@ -0,0 +1,12 @@
+-- Create a safar database if not exist and create a table login
+CREATE DATABASE IF NOT EXISTS safar;
+USE safar;
+CREATE TABLE IF NOT EXISTS login(
+	email_address VARCHAR(255) NOT NULL,
+	phone_number VARCHAR(255) NOT NULL,
+	pass_shash VARCHAR(255) NOT NULL,
+	user_id INT,
+	FOREIGN KEY (user_id) REFERENCES users (id)
+	);
+
+
